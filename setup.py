667604e362b4e9202a03e7a9c35ec4c7f0c6f9de
Richard Correro <rcorrero@stanford.edu>
2019-08-03 13:03:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='thermidor',
     packages=find_packages(),
     version='0.1',
-    description='thermidor is a Python module that streamlines the machine learning model development process. thermidor contains several utility functions and classes for use in data analysis and processing'.,
+    description='thermidor is a Python module that streamlines the machine learning model development process. thermidor contains several utility functions and classes for use in data analysis and processing.',
     author='Richard Correro',
     license='BSD-3-Clause',
 )
