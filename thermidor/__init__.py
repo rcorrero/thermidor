441ef9a0dceb63d5c30797b26361d504418d83a1
Richard Correro <rcorrero@stanford.edu>
2019-08-04 17:04:21 -0500

    Add DataFramer import

--- thermidor/__init__.py
+++ thermidor/__init__.py
@@ -7,4 +7,5 @@ thermidor is a Python module that streamlines the machine learning model develop
 # Import objects - flat is better than nested
 from thermidor.classes import ColumnSelector
 from thermidor.classes import TypeSelector
+from thermidor.classes import DataFramer
 from thermidor.functions import get_categories, correlation
